427bb9172315bee9c4ac3a33d86e2b78d8112153
root <root@vdeverde.com.ar>
2017-11-02 22:54:24 +0000

    Testing

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -65,72 +65,86 @@ class WebController extends Controller
     public function mail_sender(Request $request)
     {
 
-		$MailToAddress = "info@vdeverde.com.ar";
-		$MailSubject   = "Mensaje desde la web";
+    	
+		$MailToAddress   = "info@vdeverde.com.ar";
+		$MailSubject     = "Mensaje desde la web";
+		$MailFromAddress = "info@vdeverde.com.ar";
 
-		if (!isset($MailFromAddress) ) {
-			$MailFromAddress = "info@vdeverde.com.ar";
+
+		// if (!isset($MailFromAddress) ) {
+		// 	$MailFromAddress = "info@vdeverde.com.ar";
+		// }
+
+		 $Header  = "Contacto desde la Web";
+		 $Message = $Footer = "";
+
+		// if (!is_array($_POST))
+		// 	return;
+		// 	reset($_POST);
+
+		// // Genera un mensaje personalizado.
+		 $Message  = "Nombre/Empresa: ".stripslashes($_POST['name'])." \n";
+		 $Message .= "Tel.: ".stripslashes($_POST['tel'])." \n";
+		 $Message .= "E-Mail: ".stripslashes($_POST['email'])." \n";
+		 $Message .= "Consulta/Mensaje: ".stripslashes($_POST['message'])." \n";
+
+
+		try {
+    		mail("$MailToAddress", "$MailSubject", "$Message", "From: $MailFromAddress");
+			return response()->json(['response' => $Message]); 	
+		} catch(Exception $e) {
+			return response()->json(['response' => $e]); 
 		}
 
-		$Header  = "Contacto desde la Web";
-		$Message = $Footer = "";
-
-		if (!is_array($_POST))
-			return;
-			reset($_POST);
-
-		// Genera un mensaje personalizado.
-		$Message  = "Nombre/Empresa: ".stripslashes($_POST['name'])." \n";
-		$Message .= "Tel.: ".stripslashes($_POST['tel'])." \n";
-		$Message .= "E-Mail: ".stripslashes($_POST['email'])." \n";
-		$Message .= "Consulta/Mensaje: ".stripslashes($_POST['message'])." \n";
-
-		if ($Header) {
-			$Message = $Header."\n\n".$Message."\n\n";
-		}
-
-		// $REMOTE_USER = (isset($_SERVER["REMOTE_USER"]))?$_SERVER["REMOTE_USER"]:"";
-		$REMOTE_ADDR = (isset($_SERVER["REMOTE_ADDR"]))?$_SERVER["REMOTE_ADDR"]:"";
-		// $Message .= "REMOTE USER: ". $REMOTE_USER."\n";
-		$Message .= "I.P del contacto: ". $REMOTE_ADDR."\n";
-
-		if ($Footer) {
-			$Message .= "\n\n".$Footer;
-		}
-
-		mail( "$MailToAddress", "$MailSubject", "$Message", "From: $MailFromAddress");
+    	
 
 
-		function ValidarDatos($campo){
-			//Array con las posibles cabeceras a utilizar por un spammer
-			$badHeads = array("Content-Type:",
-			"MIME-Version:",
-			"Content-Transfer-Encoding:",
-			"Return-path:",
-			"Subject:",
-			"From:",
-			"Envelope-to:",
-			"To:",
-			"bcc:",
-			"cc:");
-			//Comprobamos que entre los datos no se encuentre alguna de
-			//las cadenas del array. Si se encuentra alguna cadena se
-			//dirige a una página de Forbidden
-			foreach($badHeads as $valor){
-				if(strpos(strtolower($campo), strtolower($valor)) !== false){
-					header( "HTTP/1.0 403 Forbidden");
-					exit;
-				}
-			}
-		}
+		// if ($Header) {
+		// 	$Message = $Header."\n\n".$Message."\n\n";
+		// }
 
-		//Ejemplo de llamadas a la funcion
-		ValidarDatos($_POST['name']);
-		ValidarDatos($_POST['email']);
-		ValidarDatos($_POST['tel']);
-		ValidarDatos($_POST['message']);
+		// // $REMOTE_USER = (isset($_SERVER["REMOTE_USER"]))?$_SERVER["REMOTE_USER"]:"";
+		// $REMOTE_ADDR = (isset($_SERVER["REMOTE_ADDR"]))?$_SERVER["REMOTE_ADDR"]:"";
+		// // $Message .= "REMOTE USER: ". $REMOTE_USER."\n";
+		// $Message .= "I.P del contacto: ". $REMOTE_ADDR."\n";
+
+		// if ($Footer) {
+		// 	$Message .= "\n\n".$Footer;
+		// }
+
+		// mail( "$MailToAddress", "$MailSubject", "$Message", "From: $MailFromAddress");
+
+
+		// function ValidarDatos($campo){
+		// 	//Array con las posibles cabeceras a utilizar por un spammer
+		// 	$badHeads = array("Content-Type:",
+		// 	"MIME-Version:",
+		// 	"Content-Transfer-Encoding:",
+		// 	"Return-path:",
+		// 	"Subject:",
+		// 	"From:",
+		// 	"Envelope-to:",
+		// 	"To:",
+		// 	"bcc:",
+		// 	"cc:");
+		// 	//Comprobamos que entre los datos no se encuentre alguna de
+		// 	//las cadenas del array. Si se encuentra alguna cadena se
+		// 	//dirige a una página de Forbidden
+		// 	foreach($badHeads as $valor){
+		// 		if(strpos(strtolower($campo), strtolower($valor)) !== false){
+		// 			header( "HTTP/1.0 403 Forbidden");
+		// 			exit;
+		// 		}
+		// 	}
+		// }
+
+		// //Ejemplo de llamadas a la funcion
+		// ValidarDatos($_POST['name']);
+		// ValidarDatos($_POST['email']);
+		// ValidarDatos($_POST['tel']);
+		// ValidarDatos($_POST['message']);
  
-		return response()->json(['response' => $Message]); 
+		
 
 		}
 
