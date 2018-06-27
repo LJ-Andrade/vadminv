984bf8a0efc60a73466c8f169d353f75776ac907
root <javzero@hotmail.com>
2018-06-27 20:28:57 -0300

    Home Images Updated | Styles updated

--- resources/views/web.blade.php
+++ resources/views/web.blade.php
@@ -29,86 +29,84 @@
     </div>
 
     {{-- --}}
-    <div class="mosaic-block">
-        <div class="container centered-info">
-            <div class="row wow animated fadeIn" data-wow-delay="300ms">
-                <h1><i>Generá un espacio chic y natural en tu casa</i></h1>
-                <p>
-                    Contamos con diferentes modelos de macetas en madera, <br> además te ayudamos a armar tu diseño a medida.
-                </p>
-            </div>
+    <div class="container mosaic-section">
+        
+        <div class="row centered-info wow animated fadeIn" data-wow-delay="300ms">
+            <h1>Generá un espacio chic y natural en tu casa</h1>
+            <p>
+                Contamos con diferentes modelos de macetas en madera, <br> además te ayudamos a armar tu diseño a medida.
+            </p>
         </div>
-        <div class="container bgimg-container">
-            <div class="row mobile-flex-column">
-                <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="200ms">
-                    <a href="{{ url('https://vdeverde.mercadoshops.com.ar/kit-para-regalos_qO27058343XtOcxSM') }}">
-                        <div class="inner maderas-img-link bg-zoom-home border">
-                            <h1 class="white-txt bt-shadow-w">KITS PARA <br>REGALOS</h1>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="300ms" >
-                    <a href="{{ url('maderas') }}">
-                    <div class="inner blue-back-link bg-zoom-home">
-                        <h1 class="white-txt bt-shadow3">NUESTRAS <br> MADERAS</h1>
+    
+        <div class="row mosaic-block">
+            <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="200ms">
+                <a href="{{ url('https://vdeverde.mercadoshops.com.ar/kit-para-regalos_qO27058343XtOcxSM') }}">
+                    <div class="inner maderas-img-link border">
+                        <h1 class="white-txt bt-shadow-w">KITS PARA <br>REGALOS</h1>
                     </div>
-                    </a>    
-                </div>
+                </a>
+            </div>
 
-                <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="400ms">
-                    <a href="{{ url('https://vdeverde.mercadoshops.com.ar/accesorios-jardin_qO27284169XtOcxSM') }}">
-                        <div class="inner accesorios-img-link bg-zoom-home">
-                            <h1 class="white-txt bt-shadow">ACCESORIOS</h1>
-                        </div>
-                    </a>
+            <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="300ms" >
+                <a href="{{ url('maderas') }}">
+                <div class="inner blue-back-link">
+                    <h1 class="white-txt bt-shadow3">NUESTRAS <br> MADERAS</h1>
                 </div>
+                </a>    
             </div>
-            <div class="row mobile-flex-column">
-                <div class="col-md-6 col-sm-6 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="500ms">
-                    <a href="{{ url('https://vdeverde.mercadoshops.com.ar/huerta-en-jardin_qO27061125XtOcxSM') }}">
-                        <div class="inner huerta-img-link bg-zoom-home txC">
-                            <h1 class="white-txt bt-shadow">DISEÑOS PARA <br> JARDINES</h1>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-6 col-sm-6 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="600ms">
-                    <a href="{{ url('https://vdeverde.mercadoshops.com.ar/huerta-urbana_qO27061017XtOcxSM') }}">
-                        <div class="inner urbana-img-link bg-zoom-home txC">
-                            <h1 class="white-txt bt-shadow">DISEÑOS PARA <br>ESPACIOS PEQUEÑOS</h1>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="row  mobile-flex-column">
-                <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="700ms">
-                    <a href="{{ url('portfolio') }}">
-                        <div class="inner blue-back-link bg-zoom-home txC">
-                            <h1 class="white-txt bt-shadow3">PORTFOLIO</h1>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="800ms">
-                    <a style="color: #fff" href="{{ url('blog') }}">
-                        <div class="inner blog-img-link bg-zoom-home">
-                            <h1 class="white-tx bt-shadow">BLOG</h1>
-                            <p class="white-tx bt-shadow">NOVEDADES / CONSEJOS</p>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="900ms">
-                    <a href="{{ url('https://play.google.com/store/apps/details?id=ar.teovogel.vdeverde') }}">
-                        <div class="inner blue-back-link bg-zoom-home">
-                            <h1 class="white-txt bt-shadow3">APP</h1>
-                            <p class="white-txt txC bt-shadow3">  
-                                Consejos para armar y mantener tu huerta (Descargala gratis acá)
-                            </p>
-                        </div>
-                    </a>
-                </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="400ms">
+                <a href="{{ url('https://vdeverde.mercadoshops.com.ar/accesorios-jardin_qO27284169XtOcxSM') }}">
+                    <div class="inner accesorios-img-link">
+                        <h1 class="white-txt bt-shadow">ACCESORIOS</h1>
+                    </div>
+                </a>
             </div>
         </div>
-    </div> {{-- Mosaic Block --}}
+        <div class="row mosaic-block">
+            <div class="col-md-6 col-sm-6 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="500ms">
+                <a href="{{ url('https://vdeverde.mercadoshops.com.ar/huerta-en-jardin_qO27061125XtOcxSM') }}">
+                    <div class="inner huerta-img-link">
+                        <h1 class="white-txt bt-shadow">DISEÑOS PARA <br> JARDINES</h1>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="600ms">
+                <a href="{{ url('https://vdeverde.mercadoshops.com.ar/huerta-urbana_qO27061017XtOcxSM') }}">
+                    <div class="inner urbana-img-link">
+                        <h1 class="white-txt bt-shadow">DISEÑOS PARA <br>ESPACIOS PEQUEÑOS</h1>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row mosaic-block">
+            <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="700ms">
+                <a href="{{ url('portfolio') }}">
+                    <div class="inner blue-back-link txC">
+                        <h1 class="white-txt bt-shadow3">PORTFOLIO</h1>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="800ms">
+                <a style="color: #fff" href="{{ url('blog') }}">
+                    <div class="inner blog-img-link">
+                        <h1 class="white-tx bt-shadow">BLOG</h1>
+                        <p class="white-tx bt-shadow">NOVEDADES / CONSEJOS</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12 bgimg-link wow animated fadeIn" data-wow-delay="900ms">
+                <a href="{{ url('https://play.google.com/store/apps/details?id=ar.teovogel.vdeverde') }}">
+                    <div class="inner blue-back-link">
+                        <h1 class="white-txt bt-shadow3">APP</h1>
+                        <p class="white-txt txC bt-shadow3">  
+                            Consejos para armar y mantener tu huerta (Descargala gratis acá)
+                        </p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div> {{-- Mosaic Section --}}
     
     {{-- --}}
     <div class="container-fluid centered-info lblue-back margTop50 wow animated fadeIn" data-wow-delay="300ms">
